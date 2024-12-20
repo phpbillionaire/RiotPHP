82e921cfb2d14fbb7998b92e496146e7c142dbc3
phpbillionaire <toruu32@gmail.com>
2024-12-21 00:38:47 +0200

    branch name

--- src/Api/ApiHandler.php
+++ src/Api/ApiHandler.php
@@ -1,55 +1,30 @@
 <?php
-namespace RiotGamesPHP\Api;
+namespace RGSDK\Api;
 
-use RiotGamesPHP\Config\ConfigInterface;
+use RGSDK\Config\Config;
 use GuzzleHttp\{Client, ClientInterface};
 
-/**
- * Handles API requests to Riot Games' API.
- */
 final class ApiHandler implements ApiHandlerInterface
 {
-    /**
-     * @var ConfigInterface Configuration instance for API settings.
-     */
-    private ConfigInterface $config;
+    private Config $config;
 
-    /**
-     * @var ClientInterface HTTP client for sending requests.
-     */
     private ClientInterface $httpClient;
 
-    /**
-     * ApiHandler constructor.
-     *
-     * @param ConfigInterface $config     Configuration for the API (e.g., region, API key).
-     * @param ClientInterface $httpClient HTTP client implementation.
-     */
-    public function __construct(ConfigInterface $config, ClientInterface $httpClient)
+    public function __construct(Config $config, ClientInterface $httpClient)
     {
         $this->config = $config;
         $this->httpClient = new Client([
-            "base_uri" => "https://{$this->config->getRegion()}.api.riotgames.com/",
+            "base_uri" => "https://{$this->config->region}.api.riotgames.com/",
             "timeout" => 5.0,
             "verify" => false,
         ]);
     }
 
-    /**
-     * Sends a GET request to the specified API endpoint.
-     *
-     * @param string $endpoint The API endpoint (relative to the base URI).
-     * @param array $params    Optional query parameters for the request.
-     *
-     * @return array|int The decoded JSON response as an associative array, or the HTTP status code.
-     *
-     * @throws \Exception If the request fails or an error occurs.
-     */
     public function request(string $endpoint, array $params = []): array|int
     {
         try {
             $response = $this->httpClient->request('GET', $endpoint, [
-                "query" => array_merge($params, ["api_key" => $this->config->getApiKey()]),
+                "query" => array_merge($params, ["api_key" => $this->config->apiKey]),
             ]);
             return json_decode($response->getBody()->getContents(), true);
         } catch (\Exception $e) {
